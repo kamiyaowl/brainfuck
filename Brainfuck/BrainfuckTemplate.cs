0558b46c97f32f5d39ccf06feac5a677be1902cb
Fumiya Katoh <katoh.numazu.ct@gmail.com>
2014-04-21 00:36:01 +0900

    Hello World!

---
+++ Brainfuck/BrainfuckTemplate.cs
@@ -0,0 +1,200 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brainfuck {
+	public static class BrainfuckTemplete {
+	
+		public const string Source = @">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-]<.>+++++++++++[<+++++>-]<.>++++++++[<+++>-]<.+++.------.--------.[-]>++++++++[<++++>-]<+.[-]++++++++++.";
+		public static void Run() {
+			var ptr = 0;
+			var buffer = new char[30000];
+				 ++ptr; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 while(buffer[ptr] != 0) {
+				 --ptr; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++ptr; 
+				--buffer[ptr];  
+				 }
+				 --ptr; 
+				 Console.Write(buffer[ptr]); 
+				 ++ptr; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 while(buffer[ptr] != 0) {
+				 --ptr; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++ptr; 
+				--buffer[ptr];  
+				 }
+				 --ptr; 
+				 ++buffer[ptr]; 
+				 Console.Write(buffer[ptr]); 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 Console.Write(buffer[ptr]); 
+				 Console.Write(buffer[ptr]); 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 Console.Write(buffer[ptr]); 
+				 while(buffer[ptr] != 0) {
+				--buffer[ptr];  
+				 }
+				 ++ptr; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 while(buffer[ptr] != 0) {
+				 --ptr; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++ptr; 
+				--buffer[ptr];  
+				 }
+				 --ptr; 
+				 Console.Write(buffer[ptr]); 
+				 ++ptr; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 while(buffer[ptr] != 0) {
+				 --ptr; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++ptr; 
+				--buffer[ptr];  
+				 }
+				 --ptr; 
+				 Console.Write(buffer[ptr]); 
+				 ++ptr; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 while(buffer[ptr] != 0) {
+				 --ptr; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++ptr; 
+				--buffer[ptr];  
+				 }
+				 --ptr; 
+				 Console.Write(buffer[ptr]); 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 Console.Write(buffer[ptr]); 
+				--buffer[ptr];  
+				--buffer[ptr];  
+				--buffer[ptr];  
+				--buffer[ptr];  
+				--buffer[ptr];  
+				--buffer[ptr];  
+				 Console.Write(buffer[ptr]); 
+				--buffer[ptr];  
+				--buffer[ptr];  
+				--buffer[ptr];  
+				--buffer[ptr];  
+				--buffer[ptr];  
+				--buffer[ptr];  
+				--buffer[ptr];  
+				--buffer[ptr];  
+				 Console.Write(buffer[ptr]); 
+				 while(buffer[ptr] != 0) {
+				--buffer[ptr];  
+				 }
+				 ++ptr; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 while(buffer[ptr] != 0) {
+				 --ptr; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++ptr; 
+				--buffer[ptr];  
+				 }
+				 --ptr; 
+				 ++buffer[ptr]; 
+				 Console.Write(buffer[ptr]); 
+				 while(buffer[ptr] != 0) {
+				--buffer[ptr];  
+				 }
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 ++buffer[ptr]; 
+				 Console.Write(buffer[ptr]); 
+			}
+	}
+}
+
